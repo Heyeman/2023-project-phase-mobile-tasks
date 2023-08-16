dd340c0f2838817b9080a73a792f52a0f1b11888
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2023-08-16 15:37:17 +0300

    Add files via upload

--- onboarding/tasks/lib/features/todo/data/repositories/todo_repository_impl.dart
+++ onboarding/tasks/lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/core/error/exceptions.dart';
 import 'package:flutter_application_1/core/error/failures.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_application_1/features/todo/domain/repositories/todo_rep
 import '../../../../core/network/network_info.dart';
 import '../datasources/todotask_local_data_source.dart';
 import '../datasources/todotask_remote_data_source.dart';
+import '../models/task_model.dart';
 
 class TodoTaskRepositoryImpl implements TodoTaskRepository {
   final TodoTaskLocalDataSource localDataSource;
@@ -19,22 +22,13 @@ class TodoTaskRepositoryImpl implements TodoTaskRepository {
       required this.networkInfo});
 
   @override
-  Future<Either<Failure, TodoTask>> completeTask(String id) {
-    // TODO: implement completeTask
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, TodoTask>> createTask(TodoTask todoTask) {
-    // TODO: implement createTask
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, List<TodoTask>>> getAllTasks() {
-    // TODO: implement getAllTasks
-    // return ;
-    throw UnimplementedError();
+  Future<Either<Failure, List<TodoTask>>> getAllTasks() async {
+    try {
+      final remoteTodoTask = await remoteDataSource.getAllTasks();
+      return Right(remoteTodoTask);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 
   @override
@@ -54,6 +48,26 @@ class TodoTaskRepositoryImpl implements TodoTaskRepository {
       } on CacheException {
         return Left(CacheFailure());
       }
+    }
+  }
+
+  @override
+  Future<Either<Failure, TodoTask>> completeTask(String id) {
+    // TODO: implement completeTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, TodoTask>> createTask(TodoTaskModel todoTaskModel) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final createdTodoTask = await remoteDataSource.createTask(TodotodoTaskModel);
+        return Right(createdTodoTask);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(ServerFailure());
     }
   }
 
--- onboarding/tasks/lib/features/todo/domain/repositories/todo_repository.dart
+++ onboarding/tasks/lib/features/todo/domain/repositories/todo_repository.dart
@@ -2,11 +2,13 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/core/error/failures.dart';
 import 'package:flutter_application_1/features/todo/domain/entities/task.dart';
 
+import '../../data/models/task_model.dart';
+
 abstract class TodoTaskRepository {
   Future<Either<Failure, List<TodoTask>>> getAllTasks();
   Future<Either<Failure, TodoTask>> getTask(String id);
   Future<Either<Failure, TodoTask>> updateTask(String id);
   Future<Either<Failure, TodoTask>> completeTask(String id);
 
-  Future<Either<Failure, TodoTask>> createTask(TodoTask todoTask);
+  Future<Either<Failure, TodoTask>> createTask(TodoTaskModel todoTask);
 }
